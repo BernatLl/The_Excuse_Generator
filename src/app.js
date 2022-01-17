7c47e2af52e07d99f9d9c916cd50f4066ba7881d
BernatLl <blloret@hotmail.com>
2022-01-17 11:40:30 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 window.onload = function() {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
+  const who = ["The dog", "A raccoon", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "abduct", "crushed", "broke"];
+  const what = ["my homework", "my laptop", "my brain", "the keys", "the car"];
+  const when = [
     "before the class",
     "right on time",
     "when I finished",
     "during my lunch",
     "while I was praying"
   ];
-  let select = array => {
-    let selectarray = Math.floor(Math.random() * array.length);
-    return array[selectarray];
+  const excuse = [who, action, what, when];
+  const RandomIndex = arr => Math.floor(Math.random() * arr.length);
+  const RandomItem = (arr, i) => arr[i];
+  const GenerateString = arr => RandomItem(arr, RandomIndex(arr));
+  const generateExcuse = arr => {
+    let str = "";
+    for (const val of arr) {
+      str += GenerateString(val) + " ";
+    }
+    return str;
   };
-
-  console.log(
-    `${select(who)} ${select(action)} ${select(what)} ${select(when)}`
-  );
+  console.log(generateExcuse(excuse));
 };
