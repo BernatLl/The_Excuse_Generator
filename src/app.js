595d19fd40015e7ce0a8d22dc4c5c2e3e5c8bc27
BernatLl <blloret@hotmail.com>
2022-01-18 09:41:14 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,24 +1,27 @@
+const who = ["The dog", "A raccoon", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "abduct", "crushed", "broke"];
+const what = ["my homework", "my laptop", "my brain", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+//Unión de los Arrays.
+const dataset = [who, action, what, when];
+//Función para numero de item aleatorio del array.
+const RandomNumber = arr => Math.floor(Math.random() * arr.length);
+//Función para sacar el valor del item
+const GetItem = (arr, i) => arr[i];
+//Sacar el string del array.
+const GetString = v => `${GetItem(v, RandomNumber(v))} `;
+//Función para generar la excusa aleatoria.
+const excuseGenerator = dset =>
+  dset.reduce((acc = "", item) => (acc += GetString(item)));
+//Selección del tag de HTML para aplicar la función.
+const paragraf = document.querySelector("#excuse");
 window.onload = function() {
-  const who = ["The dog", "A raccoon", "My grandma", "His turtle", "My bird"];
-  const action = ["ate", "peed", "abduct", "crushed", "broke"];
-  const what = ["my homework", "my laptop", "my brain", "the keys", "the car"];
-  const when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  const excuse = [who, action, what, when];
-  const RandomIndex = arr => Math.floor(Math.random() * arr.length);
-  const RandomItem = (arr, i) => arr[i];
-  const GenerateString = arr => RandomItem(arr, RandomIndex(arr));
-  const generateExcuse = arr => {
-    let str = "";
-    for (const val of arr) {
-      str += GenerateString(val) + " ";
-    }
-    return str;
-  };
-  console.log(generateExcuse(excuse));
+  //Muestra de la excusa aletaria cada vez que se actualiza la página.
+  paragraf.innerHTML = excuseGenerator(dataset);
 };
